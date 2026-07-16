5cd6c04598f22c37a5bd4b9c2164d5d039c28ede
Isac <isaacalvarez0516@gmail.com>
2026-07-16 14:14:55 -0500

    a

--- Back/database/scripts/tables/usuarios_sistema.sql
+++ Back/database/scripts/tables/usuarios_sistema.sql
@@ -1,24 +1,4 @@
-/*
- Navicat Premium Dump SQL
 
- Source Server         : Zoe-Care.db
- Source Server Type    : PostgreSQL
- Source Server Version : 170010 (170010)
- Source Host           : localhost:5432
- Source Catalog        : zoe_care_db
- Source Schema         : public
-
- Target Server Type    : PostgreSQL
- Target Server Version : 170010 (170010)
- File Encoding         : 65001
-
- Date: 13/07/2026 15:00:26
-*/
-
-
--- ----------------------------
--- Table structure for usuarios_sistema
--- ----------------------------
 DROP TABLE IF EXISTS "public"."usuarios_sistema";
 CREATE TABLE "public"."usuarios_sistema" (
   "id_usuario" int4 NOT NULL DEFAULT nextval('usuarios_sistema_id_usuario_seq'::regclass),
@@ -30,23 +10,14 @@ CREATE TABLE "public"."usuarios_sistema" (
 )
 ;
 
--- ----------------------------
--- Uniques structure for table usuarios_sistema
--- ----------------------------
+
 ALTER TABLE "public"."usuarios_sistema" ADD CONSTRAINT "usuarios_sistema_correo_electronico_key" UNIQUE ("correo_electronico");
 ALTER TABLE "public"."usuarios_sistema" ADD CONSTRAINT "usuarios_sistema_id_profecional_key" UNIQUE ("id_profecional");
 
--- ----------------------------
--- Checks structure for table usuarios_sistema
--- ----------------------------
+
 ALTER TABLE "public"."usuarios_sistema" ADD CONSTRAINT "usuarios_sistema_rol_check" CHECK (rol::text = ANY (ARRAY['Administrador'::character varying, 'Profecional'::character varying, 'Cuidador'::character varying]::text[]));
 
--- ----------------------------
--- Primary Key structure for table usuarios_sistema
--- ----------------------------
+
 ALTER TABLE "public"."usuarios_sistema" ADD CONSTRAINT "usuarios_sistema_pkey" PRIMARY KEY ("id_usuario");
 
--- ----------------------------
--- Foreign Keys structure for table usuarios_sistema
--- ----------------------------
 ALTER TABLE "public"."usuarios_sistema" ADD CONSTRAINT "usuarios_sistema_id_profecional_fkey" FOREIGN KEY ("id_profecional") REFERENCES "public"."profecionales" ("id_profecional") ON DELETE SET NULL ON UPDATE NO ACTION;
