0ce5672ecea70c8315bb2d924fb822de84f6d733
Isac <isaacalvarez0516@gmail.com>
2026-07-16 19:06:32 -0500

    add: se hizo deploy a la DB

--- Back/database/scripts/tables/usuarios_sistema.sql
+++ Back/database/scripts/tables/usuarios_sistema.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE usuarios_sistema(
      id_usuario SERIAL NOT NULL,
     correo_electronico text NOT NULL,
@@ -8,7 +9,7 @@ CREATE TABLE usuarios_sistema(
     nombre text ,
     PRIMARY KEY(id_usuario) ,
     CONSTRAINT usuarios_sistema_id_profecional_fkey FOREIGN key(id_profecional) REFERENCES profecionales(id_profecional) ,
-    CONSTRAINT usuarios_sistema_rol_check CHECK ((rol)::text = ANY ((ARRAY['Administrador'::character varying, 'Profecional'::character varying, 'Cuidador'::character varying])::text[])) 
+    CONSTRAINT usuarios_sistema_rol_check CHECK ((rol)::text = ANY ((ARRAY['Administrador'::character varying, 'Profesional'::character varying, 'Cuidador'::character varying])::text[])) 
 ); 
 CREATE UNIQUE INDEX usuarios_sistema_correo_electronico_key ON public.usuarios_sistema USING btree (correo_electronico);
 CREATE UNIQUE INDEX usuarios_sistema_id_profecional_key ON public.usuarios_sistema USING btree (id_profecional);
